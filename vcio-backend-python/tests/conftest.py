3dd5e585de97e2b2ba3c5dd448b9cc36e057b62c
Anuj Deshmukh <anujdeshmukh@hotmail.co.uk>
2026-05-18 13:27:22 +0200

    fixed conf test

--- vcio-backend-python/tests/conftest.py
+++ vcio-backend-python/tests/conftest.py
@@ -1,22 +1,33 @@
 import pytest
 
-from app import create_app
+from flask import Flask
+
 from app.extensions import db
+from app.routes.chat import chat_bp
+from app.routes.licences import licences_bp
 
 
 @pytest.fixture
 def app():
 
-    app = create_app()
+    app = Flask(__name__)
 
     app.config.update({
         "TESTING": True,
-        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:"
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
+        "SQLALCHEMY_TRACK_MODIFICATIONS": False
     })
+
+    db.init_app(app)
+
+    app.register_blueprint(chat_bp)
+    app.register_blueprint(licences_bp)
 
     with app.app_context():
         db.create_all()
+
         yield app
+
         db.session.remove()
         db.drop_all()
 
